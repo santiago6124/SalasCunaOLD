58478ca68ced79796648287fc36c186de3d0d0ee
facuolivamar <facundoolivam@gmail.com>
2023-04-18 14:38:56 -0300

    models terminado

--- migrarData/data/admin.py
+++ migrarData/data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Padron)
+admin.site.register(Fecha)
+admin.site.register(Fecha_Padron)
--- migrarData/data/models.py
+++ migrarData/data/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Padron(models.Model):
+    CSala = models.CharField(max_length=5)
+    sector = models.CharField(max_length=30)
+    nombre_sala = models.CharField(max_length=30)
+    entidad_municipal = models.CharField(max_length=30)
+    TS_a_cargo = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f"{self.CSala} {self.sector} {self.nombre_sala} {self.entidad_municipal} {self.TS_a_cargo} "
+
+class Fecha(models.Model):
+    fecha = models.CharField(max_length=30)    
+
+    def __str__(self):
+        return f"{self.fecha}"
+
+class Fecha_Padron(models.Model):
+    CSala = models.ForeignKey( Padron, on_delete=models.CASCADE )
+    Fecha = models.ForeignKey( Fecha, on_delete=models.CASCADE )
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.CSala} {self.Fecha} {self.valor} "
